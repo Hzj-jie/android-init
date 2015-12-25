2a7da7dfe84ab0307336cb55d5d3661fe00a241a
zijiehe <zijiehe@google.com>
2015-12-25 14:50:26 +0800

    Use am start -W instead of sleep 30

--- sample/init.sh
+++ sample/init.sh
@@ -4,12 +4,9 @@ echo Start init.sh
 
 sleep 120
 
-am start --user 0 com.tencent.mm/.ui.LauncherUI
-sleep 30
-am start --user 0 com.microsoft.skydrive/.MainActivity
-sleep 30
-am start --user 0 com.skype.polaris/com.skype.raider.Main
-sleep 30
+am start -W --user 0 com.tencent.mm/.ui.LauncherUI
+am start -W --user 0 com.microsoft.skydrive/.MainActivity
+am start -W --user 0 com.skype.polaris/com.skype.raider.Main
 
 sh /sdcard/init/move_wechat_pics.sh &
 sh /sdcard/init/logger.sh
