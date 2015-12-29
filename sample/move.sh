d1181fcc0e58606848511cebef75945e0017b513
Hzj_jie <hzj_jie@hotmail.com>
2015-12-30 02:11:36 +0800

    Update move.sh to match WeChat English version

--- sample/move.sh
+++ sample/move.sh
@@ -1,11 +1,15 @@
 #!/system/bin/sh
 
 touch /storage/emulated/0/tencent/MicroMsg/WeiXin/do.not.delete
+touch /storage/emulated/0/tencent/MicroMsg/WeChat/do.not.delete
 
 while (true);
 do
     mv /storage/emulated/0/tencent/MicroMsg/WeiXin/* /storage/sdcard1/DCIM/WeChat/
     mv /storage/sdcard1/DCIM/WeChat/do.not.delete /storage/emulated/0/tencent/MicroMsg/WeiXin/
+
+    mv /storage/emulated/0/tencent/MicroMsg/WeChat/* /storage/sdcard1/DCIM/WeChat/
+    mv /storage/sdcard1/DCIM/WeChat/do.not.delete /storage/emulated/0/tencent/MicroMsg/WeChat/
 
     mv /storage/sdcard1/HDWallPaper/bigImage/* '/storage/sdcard1/background images/'
     mv /storage/sdcard1/Pictures/OGQ/* '/storage/sdcard1/background images/'
