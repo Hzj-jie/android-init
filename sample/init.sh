326236726f469535cf371f25b85634198fd015cd
zijiehe <zijiehe@google.com>
2015-12-29 15:40:10 +0800

    Update sample/init.sh, do not need to debug any more

--- sample/init.sh
+++ sample/init.sh
@@ -8,7 +8,10 @@ am start -W --user 0 com.tencent.mm/.ui.LauncherUI
 am start -W --user 0 com.microsoft.skydrive/.MainActivity
 am start -W --user 0 com.skype.polaris/com.skype.raider.Main
 
-sh /storage/sdcard1/init/move.sh &
-sh /storage/sdcard1/init/logger.sh
+# sh /storage/sdcard1/init/move.sh &
+# sh /storage/sdcard1/init/logger.sh
+
+# Do not need to debug anymore
+sh /storage/sdcard1/init/move.sh
 
 echo Finish init.sh
