4baf77f9fc2f32d775bb091978741fc2b0f177fe
zijiehe <zijiehe@google.com>
2016-02-20 20:30:04 +0800

    Move WeChat images to Pictures folder, instead of DCIM

--- sample/move.sh
+++ sample/move.sh
@@ -3,16 +3,19 @@
 touch /storage/emulated/0/tencent/MicroMsg/WeiXin/do.not.delete
 touch /storage/emulated/0/tencent/MicroMsg/WeChat/do.not.delete
 
+mkdir -p /storage/sdcard1/Pictures/WeChat
+mkdir -p '/storage/sdcard1/Pictures/background images/'
+
 while (true);
 do
-    mv /storage/emulated/0/tencent/MicroMsg/WeiXin/* /storage/sdcard1/DCIM/WeChat/
-    mv /storage/sdcard1/DCIM/WeChat/do.not.delete /storage/emulated/0/tencent/MicroMsg/WeiXin/
+    mv /storage/emulated/0/tencent/MicroMsg/WeiXin/* /storage/sdcard1/Pictures/WeChat/
+    mv /storage/sdcard1/Pictures/WeChat/do.not.delete /storage/emulated/0/tencent/MicroMsg/WeiXin/
 
-    mv /storage/emulated/0/tencent/MicroMsg/WeChat/* /storage/sdcard1/DCIM/WeChat/
-    mv /storage/sdcard1/DCIM/WeChat/do.not.delete /storage/emulated/0/tencent/MicroMsg/WeChat/
+    mv /storage/emulated/0/tencent/MicroMsg/WeChat/* /storage/sdcard1/Pictures/WeChat/
+    mv /storage/sdcard1/Pictures/WeChat/do.not.delete /storage/emulated/0/tencent/MicroMsg/WeChat/
 
-    mv /storage/sdcard1/HDWallPaper/bigImage/* '/storage/sdcard1/background images/'
-    mv /storage/sdcard1/Pictures/OGQ/* '/storage/sdcard1/background images/'
+    mv /storage/sdcard1/HDWallPaper/bigImage/* '/storage/sdcard1/Pictures/background images/'
+    mv /storage/sdcard1/Pictures/OGQ/* '/storage/sdcard1/Pictures/background images/'
 
     mv /storage/emulated/0/Download/* /storage/sdcard1/Download/
     
