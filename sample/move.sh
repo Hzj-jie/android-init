5497ee002a4d43a745a0c27e2dd92add46c762e7
zijiehe <zijiehe@google.com>
2016-02-20 23:31:21 +0800

    Better move.sh

--- sample/move.sh
+++ sample/move.sh
@@ -1,27 +1,33 @@
 #!/system/bin/sh
 
-touch /storage/emulated/0/tencent/MicroMsg/WeiXin/do.not.delete
-touch /storage/emulated/0/tencent/MicroMsg/WeChat/do.not.delete
-
 mkdir -p /storage/sdcard1/Pictures/WeChat
 mkdir -p '/storage/sdcard1/Pictures/background images/'
+mkdir -p /storage/sdcard1/Pictures/MagazineUnlock/
 
-while (true);
+while (true)
 do
-    mv /storage/emulated/0/tencent/MicroMsg/WeXin/*.mp4 /storage/sdcard1/DCIM/
-    mv /storage/emulated/0/tencent/MicroMsg/WeiXin/* /storage/sdcard1/Pictures/WeChat/
-    mv /storage/sdcard1/Pictures/WeChat/do.not.delete /storage/emulated/0/tencent/MicroMsg/WeiXin/
+    if [ -d /storage/emulated/0/tencent/MicroMsg/WeXin ]
+    then
+        mv /storage/emulated/0/tencent/MicroMsg/WeXin/*.mp4 /storage/sdcard1/DCIM/
+        mv /storage/emulated/0/tencent/MicroMsg/WeiXin/* /storage/sdcard1/Pictures/WeChat/
+        touch /storage/emulated/0/tencent/MicroMsg/WeiXin/do.not.delete
+    fi
 
-    mv /storage/emulated/0/tencent/MicroMsg/WeChat/*.mp4 /storage/sdcard1/DCIM/
-    mv /storage/emulated/0/tencent/MicroMsg/WeChat/* /storage/sdcard1/Pictures/WeChat/
-    mv /storage/sdcard1/Pictures/WeChat/do.not.delete /storage/emulated/0/tencent/MicroMsg/WeChat/
+    if [ -d /storage/emulated/0/tencent/MicroMsg/WeChat ]
+    then
+        mv /storage/emulated/0/tencent/MicroMsg/WeChat/*.mp4 /storage/sdcard1/DCIM/
+        mv /storage/emulated/0/tencent/MicroMsg/WeChat/* /storage/sdcard1/Pictures/WeChat/
+        touch /storage/emulated/0/tencent/MicroMsg/WeChat/do.not.delete
+    fi
+
+    rm /storage/sdcard1/Pictures/WeChat/do.not.delete
 
     mv /storage/sdcard1/HDWallPaper/bigImage/* '/storage/sdcard1/Pictures/background images/'
     mv /storage/sdcard1/Pictures/OGQ/* '/storage/sdcard1/Pictures/background images/'
 
     mv /storage/emulated/0/Download/* /storage/sdcard1/Download/
     mv /storage/emulated/0/Pictures/Screenshots/* /storage/sdcard1/Pictures/Screenshots/
-    cp /storage/emulated/0/MagazineUnlock/* /storage/sdcard1/Pictures/MagazineUnlock/
+    sh /storage/sdcard1/init/copy-new.sh /storage/emulated/0/MagazineUnlock/ /storage/sdcard1/Pictures/MagazineUnlock/
 
     sleep 1800
-done;
+done
