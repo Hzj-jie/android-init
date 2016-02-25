b3f38ee45f3cfe21ad437f1af4674bb087a4030f
zijiehe <zijiehe@google.com>
2016-02-25 17:53:43 +0800

    Remove skydrive and skype from auto-start list

--- sample/init.sh
+++ sample/init.sh
@@ -5,8 +5,8 @@ echo Start init.sh
 sleep 120
 
 sh /storage/sdcard1/init/start-activity.sh "com.tencent.mm" ".ui.LauncherUI"
-sh /storage/sdcard1/init/start-activity.sh "com.microsoft.skydrive" ".MainActivity"
-sh /storage/sdcard1/init/start-activity.sh "com.skype.polaris" "com.skype.raider.Main"
+# sh /storage/sdcard1/init/start-activity.sh "com.microsoft.skydrive" ".MainActivity"
+# sh /storage/sdcard1/init/start-activity.sh "com.skype.polaris" "com.skype.raider.Main"
 
 # sh /storage/sdcard1/init/move.sh &
 # sh /storage/sdcard1/init/logger.sh
