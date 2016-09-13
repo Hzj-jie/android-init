4b760c6aa82ed4df96ae67b3c98f40cc0df8f379
Zijie He <zijiehe@chromium.org>
2016-09-13 16:47:26 -0700

    move gps-on from screen-on to screen-unlock

--- sample/screen-on.sh
+++ sample/screen-on.sh
@@ -1,11 +1,3 @@
 #!/system/bin/sh
 
 echo in screen-on.sh
-
-if [ -z "$CURRENT" ]
-then
-    echo No CURRENT defined, detect current script location
-    export CURRENT=$(dirname "$(readlink -f "$0")")
-fi
-
-sh "$CURRENT/gps-on.sh"
--- sample/screen-unlock.sh
+++ sample/screen-unlock.sh
@@ -1,3 +1,11 @@
 #!/system/bin/sh
 
 echo in screen-unlock.sh
+
+if [ -z "$CURRENT" ]
+then
+    echo No CURRENT defined, detect current script location
+    export CURRENT=$(dirname "$(readlink -f "$0")")
+fi
+
+sh "$CURRENT/gps-on.sh"
