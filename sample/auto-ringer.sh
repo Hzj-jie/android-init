2191762a0f3edfb528bab4784dd0a88c459530f1
Hzj_jie <hzj_jie@hotmail.com>
2016-09-14 03:51:50 -0700

    syntax error in auto-ringer.sh

--- sample/auto-ringer.sh
+++ sample/auto-ringer.sh
@@ -6,21 +6,21 @@ do
   MINUTE=$(date +%M)
 
   if [ "$HOUR" -eq 23 -a "$MINUTE" -ge 0 -a "$MINUTE" -le 30 ]
+  then
     # 23:00 - 08:00 -- vibrate mode
     sh "$CURRENT/ringer-vibrate.sh"
-  then
   elif [ "$HOUR" -eq 8 -a "$MINUTE" -ge 0 -a "$MINUTE" -le 30 ]
+  then
     # 08:00 - 10:29 -- normal mode
     sh "$CURRENT/ringer-normal.sh"
-  then
   elif [ "$HOUR" -eq 10 -a "$MINUTE" -ge 29 -a "$MINUTE" -le 59 ]
+  then
     # 10:29 - 18:29 -- vibrate mode
     sh "$CURRENT/ringer-vibrate.sh"
-  then
   elif [ "$HOUR" -eq 18 -a "$MINUTE" -ge 29 -a "$MINUTE" -le 59 ]
+  then
     # 18:29 - 23:00 -- normal mode
     sh "$CURRENT/ringer-normal.sh"
-  then
   fi
 
   sleep 900
