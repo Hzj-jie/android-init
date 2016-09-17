94878c4756824f5acc4bc02b393e10c0c9744363
Hzj_jie <hzj_jie@hotmail.com>
2016-09-17 01:08:10 -0700

    Use a different strategy to execute media-scanner

--- sample/media-scanner.sh
+++ sample/media-scanner.sh
@@ -2,8 +2,13 @@
 
 while true
 do
-    sleep 21600
+  HOUR=$(date +%H)
 
+  if [ "$HOUR" -ge 3 -o "$HOUR" -le 5 ]
+  then
     sh "$CURRENT/start-activity.sh" org.gemini.media_scanner .MediaScannerActivity
     sh "$CURRENT/wait-process.sh" "org.gemini.media_scanner"
+  fi
+
+  sleep 3600
 done
