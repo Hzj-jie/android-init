9c7a277e07cbdc3d4d15ca1a483a2d8cb45258c6
Hzj_jie <hzj_jie@hotmail.com>
2016-09-17 01:03:47 -0700

    Sleep before executing media-scanner, so it won't start at boot completed.

--- sample/media-scanner.sh
+++ sample/media-scanner.sh
@@ -2,8 +2,8 @@
 
 while true
 do
+    sleep 21600
+
     sh "$CURRENT/start-activity.sh" org.gemini.media_scanner .MediaScannerActivity
     sh "$CURRENT/wait-process.sh" "org.gemini.media_scanner"
-
-    sleep 21600
 done
