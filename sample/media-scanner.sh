bbc2989f4059edd63a8efc8ad2469828d2515c42
Hzj_jie <hzj_jie@hotmail.com>
2016-09-17 01:14:18 -0700

    typo

--- sample/media-scanner.sh
+++ sample/media-scanner.sh
@@ -4,7 +4,7 @@ while true
 do
   HOUR=$(date +%H)
 
-  if [ "$HOUR" -ge 3 -o "$HOUR" -le 5 ]
+  if [ "$HOUR" -ge 3 -a "$HOUR" -le 5 ]
   then
     sh "$CURRENT/start-activity.sh" org.gemini.media_scanner .MediaScannerActivity
     sh "$CURRENT/wait-process.sh" "org.gemini.media_scanner"
