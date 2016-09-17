b8dce674b64102ace6a19d18b90e0427f0a5ea4a
Hzj_jie <hzj_jie@hotmail.com>
2016-09-16 23:52:17 -0700

    Add stop-auto-ringer check

--- sample/auto-ringer.sh
+++ sample/auto-ringer.sh
@@ -2,34 +2,39 @@
 
 while (true)
 do
-  HOUR=$(date +%H)
-  MINUTE=$(date +%M)
-  DAY=$(date +%u)
-
-  if [ "$HOUR" -ge 23 -o "$HOUR" -le 7 ]
+  if [ -a "$CURRENT/stop-auto-ringer" ]
   then
-    # 23:00 - 08:00 -- vibrate mode
-    sh "$CURRENT/ringer-vibrate.sh"
+    echo "stop-auto-ringer"
   else
-    if [ "$DAY" -ge 1 -a "$DAY" -le 5 ]
+    HOUR=$(date +%H)
+    MINUTE=$(date +%M)
+    DAY=$(date +%u)
+
+    if [ "$HOUR" -ge 23 -o "$HOUR" -le 7 ]
     then
-      # working day
-      if [ "$HOUR" -ge 8 -a "$HOUR" -le 9 ]
+      # 23:00 - 08:00 -- vibrate mode
+      sh "$CURRENT/ringer-vibrate.sh"
+    else
+      if [ "$DAY" -ge 1 -a "$DAY" -le 5 ]
       then
-        # 08:00 - 10:00 -- normal mode
-        sh "$CURRENT/ringer-normal.sh"
-      elif [ "$HOUR" -ge 10 -a "$HOUR" -le 17 ]
-      then
-        # 10:00 - 18:00 -- vibrate mode
-        sh "$CURRENT/ringer-vibrate.sh"
-      elif [ "$HOUR" -ge 18 -a "$HOUR" -le 22 ]
-      then
-        # 18:00 - 23:00 -- normal mode
+        # working day
+        if [ "$HOUR" -ge 8 -a "$HOUR" -le 9 ]
+        then
+          # 08:00 - 10:00 -- normal mode
+          sh "$CURRENT/ringer-normal.sh"
+        elif [ "$HOUR" -ge 10 -a "$HOUR" -le 17 ]
+        then
+          # 10:00 - 18:00 -- vibrate mode
+          sh "$CURRENT/ringer-vibrate.sh"
+        elif [ "$HOUR" -ge 18 -a "$HOUR" -le 22 ]
+        then
+          # 18:00 - 23:00 -- normal mode
+          sh "$CURRENT/ringer-normal.sh"
+        fi
+      else
+        # 08:00 - 23:00 -- normal mode
         sh "$CURRENT/ringer-normal.sh"
       fi
-    else
-      # 08:00 - 23:00 -- normal mode
-      sh "$CURRENT/ringer-normal.sh"
     fi
   fi
 
