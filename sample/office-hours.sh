90ec6429194e455b73c09f9c6f9dadc76d69bc9a
Zijie He <zijiehe@chromium.org>
2017-04-14 16:19:24 -0700

    Reduce office-hours

--- sample/office-hours.sh
+++ sample/office-hours.sh
@@ -9,7 +9,7 @@ DAY=$(date +%u)
 if [ "$DAY" -ge 1 -a "$DAY" -le 5 ]
 then
   # working day
-  if [ "$HOUR" -ge 10 -a "$HOUR" -le 18 ]
+  if [ "$HOUR" -ge 10 -a "$HOUR" -le 17 ]
   then
     exit 1
   fi
