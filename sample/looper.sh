aee127770937bc866db00908df2f0bf7556fe5e7
Hzj_jie <Hzj_jie@hotmail.com>
2017-04-21 00:56:52 -0700

    Update looper.sh

--- sample/looper.sh
+++ sample/looper.sh
@@ -6,6 +6,6 @@ sh ./auto-wifi.sh &
 sh ./media-scanner.sh &
 sh ./move.sh &
 
-# 1800 is the maximum reliable seconds we can sleep, otherwise the delay may
+# 1200 is the maximum reliable seconds we can sleep, otherwise the delay may
 # be too significant.
-sleep 1800
+sleep 1200
