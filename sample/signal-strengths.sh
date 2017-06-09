356aafdbd7c867ae79b87a73248905598602b5a0
Hzj_jie <hzj_jie@hotmail.com>
2017-06-09 09:42:16 -0700

    More conditions in signal-strengths.sh

--- sample/signal-strengths.sh
+++ sample/signal-strengths.sh
@@ -2,7 +2,24 @@
 
 echo in signal-strengths.sh
 
-if [ "$USER_PRESENT" != "true" ] && [ "$SIGNAL_STRENGTH" -le 1 ]
+if [ "$USER_PRESENT" == "true" ]
 then
-  sh ./turn-on-wifi-if-no-network.sh
+  if [ "WIIF_ON" == "false" ] then
+    sh ./turn-on-wifi-if-no-network.sh
+  fi
+else
+  if [ "WIFI_ON" == "true" ] then
+    if [ "WIFI_CONNECT" == "false" ] then
+      sh ./turn-off-wifi.sh
+    else
+      if [ "$SIGNAL_STRENGTH" -gt 3 ] then
+        sh ./turn-on-off-wifi-based-on-network-state.sh
+      fi
+    fi
+  else
+    # Only execute ping when signal strength is weak to save battery.
+    if [ "$SIGNAL_STRENGTH" -le 1 ] then
+      sh ./turn-on-wifi-if-no-network.sh
+    fi
+  fi
 fi
--- sample/turn-on-off-based-on-network-state.sh
+++ sample/turn-on-off-based-on-network-state.sh
@@ -4,4 +4,6 @@ sh ./mobile-data-accessible.sh
 if [ $? -eq 0 ]
 then
   sh ./turn-on-wifi.sh
+else
+  sh ./turn-off-wifi.sh
 fi
