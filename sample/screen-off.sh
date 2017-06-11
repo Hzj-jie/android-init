439412e8439bec0cc3164b1e835c434197ac4c92
Zijie He <zijiehe@chromium.org>
2017-06-11 16:43:08 -0700

    Disable enable-2g in screen-off.sh

--- sample/screen-off.sh
+++ sample/screen-off.sh
@@ -5,5 +5,6 @@ echo in screen-off.sh
 sh "./gps-off.sh" &
 sh "./change-wallpaper.sh" &
 sh "./auto-ringer.sh" &
-sh "./enable-2g.sh" &
+# sh "./enable-2g.sh" &
+sh "./signal-strength.sh" &
 sh "./turn-off-wifi.sh" &
