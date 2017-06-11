b18d2b916ad2640cc6f1d3a8589a8661de9468d4
Zijie He <zijiehe@chromium.org>
2017-06-11 15:18:20 -0700

    Enable 3g / 4g only when user is not present.

--- sample/screen-on.sh
+++ sample/screen-on.sh
@@ -6,18 +6,19 @@ if [ "$WIFI_CONNECT" == "true" ]
 then
   exit 0
 else
-  if [ "$WIFI_ON" == "true" ]
-  then
-    # No WIFI
-    sh ./turn-off-wifi.sh
-    sh ./choose-3g-4g.sh
-  else
-    if [ "$SIGNAL_STRENGTH" -le 1 ]
-    then
-      sh ./enable-2g.sh
-      sh ./turn-on-wifi.sh
-    else
-      sh ./choose-3g-4g.sh
-    fi
-  fi
+#  if [ "$WIFI_ON" == "true" ]
+#  then
+#    # No WIFI
+#    sh ./turn-off-wifi.sh
+#    sh ./choose-3g-4g.sh
+#  else
+#    if [ "$SIGNAL_STRENGTH" -le 1 ]
+#    then
+#      sh ./enable-2g.sh
+#      sh ./turn-on-wifi.sh
+#    else
+#      sh ./choose-3g-4g.sh
+#    fi
+#  fi
+  sh ./signal-strengths.sh
 fi
--- sample/signal-strengths.sh
+++ sample/signal-strengths.sh
@@ -2,24 +2,46 @@
 
 echo in signal-strengths.sh
 
-if [ "$USER_PRESENT" == "true" ]
+# if [ "$USER_PRESENT" == "true" ]
+# then
+#   if [ "$WIIF_ON" == "false" ] then
+#     sh ./turn-on-wifi-if-no-network.sh
+#   fi
+# else
+#   if [ "$WIFI_ON" == "true" ] then
+#     if [ "$WIFI_CONNECT" == "false" ] then
+#       sh ./turn-off-wifi.sh
+#     else
+#       if [ "$SIGNAL_STRENGTH" -gt 3 ] then
+#         sh ./turn-on-off-wifi-based-on-network-state.sh
+#       fi
+#     fi
+#   else
+#     # Only execute ping when signal strength is weak to save battery.
+#     if [ "$SIGNAL_STRENGTH" -le 1 ] then
+#       sh ./turn-on-wifi-if-no-network.sh
+#     fi
+#   fi
+# fi
+
+if [ "SIGNAL_STRENGTH" -le 0 ]
 then
-  if [ "WIIF_ON" == "false" ] then
-    sh ./turn-on-wifi-if-no-network.sh
-  fi
-else
-  if [ "WIFI_ON" == "true" ] then
-    if [ "WIFI_CONNECT" == "false" ] then
-      sh ./turn-off-wifi.sh
-    else
-      if [ "$SIGNAL_STRENGTH" -gt 3 ] then
-        sh ./turn-on-off-wifi-based-on-network-state.sh
+  sh ./enable-2g.sh
+  if [ "$WIFI_ON" == "false" ]
+  then
+    sh ./turn-on-wifi.sh
+  else
+    if [ "$WIFI_CONNECT" == "false" ]
+    then
+      if [ "$USER_PRESENT" == "false" ]
+      then
+        sh ./turn-off-wifi.sh
       fi
     fi
-  else
-    # Only execute ping when signal strength is weak to save battery.
-    if [ "$SIGNAL_STRENGTH" -le 1 ] then
-      sh ./turn-on-wifi-if-no-network.sh
-    fi
+  fi
+else
+  if [ "$USER_PRESENT" == "false" ]
+  then
+    sh ./choose-3g-4g.sh
   fi
 fi
