fb30b4ff0764de5401a2fbae794c7273b457eef0
Zijie He <zijiehe@chromium.org>
2017-07-14 15:20:08 -0700

    More comments in set-assistant.sh

--- sample/set-assistant.sh
+++ sample/set-assistant.sh
@@ -1,3 +1,7 @@
 #!/system/bin/sh
 
+# To use a different assistant, run:
+# am start --user 0 -d "set-assistant://org.gemini.set-assistant/<intent>" "org.gemini.set_assistant/.SetAssistantActivity"
+# intent example:
+#  com.google.android.googlequicksearchbox/com.google.android.voiceinteraction.GsaVoiceInteractionService
 am start --user 0 "org.gemini.set_assistant/.SetAssistantActivity" &
