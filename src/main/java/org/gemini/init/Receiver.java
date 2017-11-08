dd430ac39243992b3ed29eda307a2167ca1a1bd9
Hzj_jie <hzj_jie@hotmail.com>
2017-11-08 00:55:40 -0800

    Move most of the logic in to shared

--- src/main/java/org/gemini/init/Receiver.java
+++ src/main/java/org/gemini/init/Receiver.java
@@ -3,16 +3,14 @@ package org.gemini.init;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.net.Uri;
-import android.net.wifi.WifiInfo;
-import android.net.wifi.WifiManager;
 import org.gemini.shared.Event;
+import org.gemini.shared.NetworkListener;
 import org.gemini.shared.Objects;
-import org.gemini.shared.TelephonyState;
 import org.gemini.shared.PhonySignalStrengthListener;
+import org.gemini.shared.Preconditions;
+import org.gemini.shared.ScreenListener;
+import org.gemini.shared.TelephonyState;
 
 public class Receiver extends BroadcastReceiver {
   private static class Settable extends Status {
@@ -91,41 +89,91 @@ public class Receiver extends BroadcastReceiver {
   private Logger logger;
   private TelephonyState telephonyState;
   private PhonySignalStrengthListener signalStrengthListener;
+  private ScreenListener screenListener;
+  private NetworkListener networkListener;
 
   synchronized private void initialize(final Context context) {
-    if (logger == null) {
-      logger = new Logger(context, "receiver.log");
-    }
-    if (telephonyState == null) {
-      telephonyState = new TelephonyState(context);
-    }
-    if (signalStrengthListener == null) {
-      signalStrengthListener = new PhonySignalStrengthListener(context);
-      signalStrengthListener.onSignalStrength().add(
-          new Event.ParameterRunnable<Integer>() {
-            @Override
-            public void run(Integer level) {
-              broadcastSignalStrength(context, level);
-            }
-          });
-    }
-  }
+    if (logger != null) return;
 
-  private static void registerScreen(Context context) {
-    IntentFilter filter = new IntentFilter();
-    filter.addAction(Intent.ACTION_SCREEN_ON);
-    filter.addAction(Intent.ACTION_SCREEN_OFF);
-    context.registerReceiver(instance, filter);
+    Preconditions.isNull(logger);
+    logger = new Logger(context, "receiver.log");
+
+    Preconditions.isNull(telephonyState);
+    telephonyState = new TelephonyState(context);
+
+    Preconditions.isNull(signalStrengthListener);
+    signalStrengthListener = new PhonySignalStrengthListener(context);
+    signalStrengthListener.onSignalStrength().add(
+        new Event.ParameterRunnable<Integer>() {
+          @Override
+          public void run(Integer level) {
+            broadcastSignalStrength(context, level);
+          }
+        });
+
+    Preconditions.isNull(screenListener);
+    screenListener = new ScreenListener(context);
+    screenListener.onScreenOn().add(
+        new Event.ParameterRunnable<Void>() {
+          @Override
+          public void run(Void nothing) {
+            Settable.setScreenIsOn(true);
+            startService(context, Intent.ACTION_SCREEN_ON);
+          }
+        });
+    screenListener.onScreenOff().add(
+        new Event.ParameterRunnable<Void>() {
+          @Override
+          public void run(Void nothing) {
+            Settable.setScreenIsOn(false);
+            Settable.setUserIsPresenting(false);
+            startService(context, Intent.ACTION_SCREEN_OFF);
+          }
+        });
+    screenListener.onUserPresent().add(
+        new Event.ParameterRunnable<Void>() {
+          @Override
+          public void run(Void nothing) {
+            Settable.setUserIsPresenting(true);
+            startService(context, Intent.ACTION_USER_PRESENT);
+          }
+        });
+
+    Preconditions.isNull(networkListener);
+    networkListener = new NetworkListener(context);
+    networkListener.onStateChanged().add(
+        new Event.ParameterRunnable<NetworkListener.State>() {
+          @Override
+          public void run(NetworkListener.State state) {
+            Preconditions.isNotNull(state);
+            if (Settable.setWifiIsOn(state.wifiIsOn())) {
+              startService(context, state.wifiIsOn() ? WIFI_ON : WIFI_OFF);
+            }
+
+            Settable.setSsid(state.ssid());
+
+            if (Settable.setWifiIsConnected(state.wifiIsConnected())) {
+              startService(
+                  context, state.wifiIsConnected() ? WIFI_CONN : WIFI_DISCONN);
+            }
+
+            if (Settable.setMobileDataIsConnected(
+                    state.mobileDataIsConnected())) {
+              startService(context,
+                           state.mobileDataIsConnected() ?
+                               MOBILE_DATA_CONN :
+                               MOBILE_DATA_DISCONN);
+            }
+          }
+        });
   }
 
   public static void register(Context context) {
     context = context.getApplicationContext();
     instance.initialize(context);
-    registerScreen(context);
 
     // Initialize Settable.
     broadcastSignalStrength(context, PhonySignalStrengthListener.MIN_LEVEL - 1);
-    retrieveConnectionStatus(context);
   }
 
   public static void unregister(Context context) {
@@ -135,6 +183,15 @@ public class Receiver extends BroadcastReceiver {
     }
     catch (Exception ex) {}
     instance.signalStrengthListener.stop();
+    instance.screenListener.stop();
+  }
+
+  private static void startService(Context context, String action) {
+    Intent intent = new Intent(action,
+                               Uri.EMPTY,
+                               context,
+                               ExecService.class);
+    context.startService(intent);
   }
 
   private static void broadcastSignalStrength(Context context, int level) {
@@ -155,71 +212,6 @@ public class Receiver extends BroadcastReceiver {
     }
   }
 
-  private static void retrieveConnectionStatus(Context context) {
-    WifiManager wifi = (WifiManager)
-      context.getSystemService(Context.WIFI_SERVICE);
-    if (wifi.isWifiEnabled()) {
-      if (Settable.setWifiIsOn(true)) {
-        context.startService(new Intent(WIFI_ON,
-                                        Uri.EMPTY,
-                                        context,
-                                        ExecService.class));
-      }
-    }
-    else {
-      if (Settable.setWifiIsOn(false)) {
-        context.startService(new Intent(WIFI_OFF,
-                                        Uri.EMPTY,
-                                        context,
-                                        ExecService.class));
-      }
-    }
-    WifiInfo wifiInfo = wifi.getConnectionInfo();
-    if (wifiInfo != null) {
-      Settable.setSsid(wifiInfo.getSSID());
-    }
-    else {
-      Settable.setSsid("");
-    }
-    ConnectivityManager conMan = (ConnectivityManager)
-      context.getSystemService(Context.CONNECTIVITY_SERVICE);
-    NetworkInfo netInfo = conMan.getActiveNetworkInfo();
-    if (netInfo != null &&
-        netInfo.getType() == ConnectivityManager.TYPE_WIFI) {
-      if (Settable.setWifiIsConnected(true)) {
-        context.startService(new Intent(WIFI_CONN,
-                                        Uri.EMPTY,
-                                        context,
-                                        ExecService.class));
-      }
-    }
-    else {
-      if (Settable.setWifiIsConnected(false)) {
-        context.startService(new Intent(WIFI_DISCONN,
-                                        Uri.EMPTY,
-                                        context,
-                                        ExecService.class));
-      }
-    }
-    if (netInfo != null &&
-        netInfo.getType() == ConnectivityManager.TYPE_MOBILE) {
-      if (Settable.setMobileDataIsConnected(true)) {
-        context.startService(new Intent(MOBILE_DATA_CONN,
-                                        Uri.EMPTY,
-                                        context,
-                                        ExecService.class));
-      }
-    }
-    else {
-      if (Settable.setMobileDataIsConnected(false)) {
-        context.startService(new Intent(MOBILE_DATA_DISCONN,
-                                        Uri.EMPTY,
-                                        context,
-                                        ExecService.class));
-      }
-    }
-  }
-
   @Override
   public void onReceive(Context context, Intent intent)
   {
@@ -234,34 +226,8 @@ public class Receiver extends BroadcastReceiver {
 
     logger.writeLine(">>>> Received action " + intent.getAction());
     if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction()) ||
-        Intent.ACTION_SCREEN_ON.equals(intent.getAction()) ||
-        Intent.ACTION_SCREEN_OFF.equals(intent.getAction()) ||
-        Intent.ACTION_USER_PRESENT.equals(intent.getAction())) {
-      if (Intent.ACTION_SCREEN_ON.equals(intent.getAction())) {
-        Settable.setScreenIsOn(true);
-      }
-      else if (Intent.ACTION_SCREEN_OFF.equals(intent.getAction())) {
-        Settable.setScreenIsOn(false);
-        Settable.setUserIsPresenting(false);
-      }
-      else if (Intent.ACTION_USER_PRESENT.equals(intent.getAction())) {
-        Settable.setUserIsPresenting(true);
-      }
-      context.startService(new Intent(intent.getAction(),
-                                      Uri.EMPTY,
-                                      context,
-                                      ExecService.class));
-    }
-    else if (WifiManager.NETWORK_STATE_CHANGED_ACTION.equals(
-                 intent.getAction()) ||
-             WifiManager.WIFI_STATE_CHANGED_ACTION.equals(
-                 intent.getAction()) ||
-             ConnectivityManager.CONNECTIVITY_ACTION.equals(
-                 intent.getAction())) {
-      // TODO: Split retrieveConnectionStatus() into retrieveWifiStatus() and
-      // retrieveMobileDataStatus(): retrieving wifi status in
-      // CONNECTIVITY_ACTION is not necessary.
-      retrieveConnectionStatus(context);
+        "android.intent.action.QUICKBOOT_POWERON".equals(intent.getAction())) {
+      startService(context, Intent.ACTION_BOOT_COMPLETED);
     }
   }
 }
