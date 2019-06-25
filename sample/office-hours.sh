f1d85794145dcb7b8f7ceda659a92532dd168515
Zijie He <zijiehe@google.com>
2019-06-25 09:56:29 -0700

    extend office hour from 10 am to 9 am

--- sample/office-hours.sh
+++ sample/office-hours.sh
@@ -9,7 +9,7 @@ DAY=$(date +%u)
 if [ "$DAY" -ge 1 ] && [ "$DAY" -le 5 ]
 then
   # working day
-  if [ "$HOUR" -ge 10 ] && [ "$HOUR" -le 18 ]
+  if [ "$HOUR" -ge 9 ] && [ "$HOUR" -le 18 ]
   then
     exit 1
   fi
